04d67a7ce53596bebb7a97faca072f7661fbbbc1
angelikrussi <hannamrrussi@gmail.com>
2021-10-26 00:01:03 -0500

    0. Bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,24 +7,24 @@
  **/
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j,
-        int aux;
+size_t i, j;
+int auxs;
 
-	if (size < 2 || !array)
-	  return;
+if (size < 2 || !array)
+return;
 
-	for (i = 0; i < size; i++)
-	{
-		for (j = 0; j < size - 1; j++)
-		{
-			if (array[j] > array[j + 1])
-			{
-				aux = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
-				print_array(aux, size);
-			}
-		}
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size - 1; j++)
+{
+if (array[j] > array[j + 1])
+{
+auxs = array[j];
+array[j] = array[j + 1];
+array[j + 1] = auxs;
+print_array(array, size);
+}
+}
 
 	}
 }
