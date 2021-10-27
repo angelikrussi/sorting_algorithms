b564b4e9cc532aba96984930197167148ec9ba15
angelikrussi <hannamrrussi@gmail.com>
2021-10-26 21:01:22 -0500

    Header file

--- sort.h
+++ sort.h
@@ -20,8 +20,6 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-
-
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
@@ -30,4 +28,7 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
+size_t matrix(int *array, int ka, int jey, size_t tam);
+void recursivity(int *array, int ka, int jey, size_t tam);
+
 #endif
